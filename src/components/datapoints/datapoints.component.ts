5302d1e1b49e28f9af4988074c88669c3348f4b6
sagarjdhv968 <sagar.jadhav@metamagic.in>
2017-08-18 19:11:45 +0530

    datapoint style changed

--- src/components/datapoints/datapoints.component.ts
+++ src/components/datapoints/datapoints.component.ts
@@ -10,8 +10,8 @@ import {
 
 @Component({
   selector: 'amexio-datapoints',
-  template: `
-    <div style="padding: 5px;">
+  template: `    
+    <div style="padding-top: 5px">
     <table width="100%"  [style.background-color]="backgroundColor" [style.color]="fontColor">
       <tr *ngIf="north">
         <td [attr.colspan]="colspan">
@@ -36,33 +36,6 @@ import {
       </tr>
     </table>
     </div>
-    <div style="padding: 5px;">
-      <table width="100%"  [style.background-color]="backgroundColor" [style.color]="fontColor">
-        <tr *ngIf="north">
-          <td [attr.colspan]="colspan">
-            <ng-content select="amexio-north"></ng-content>
-          </td>
-        </tr>
-        <tr>
-          <td *ngIf="west">
-            <ng-content select="amexio-west"></ng-content>
-          </td>
-          <td *ngIf="center">
-            <ng-content select="amexio-center"></ng-content>
-          </td>
-          <td *ngIf="east">
-            <ng-content select="amexio-east"></ng-content>
-          </td>
-        </tr>
-        <tr  *ngIf="south">
-          <td [attr.colspan]="colspan">
-            <ng-content select="amexio-south"></ng-content>
-          </td>
-        </tr>
-      </table>
-    </div>
-
-
 
   `
 })
