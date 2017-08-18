a292cb355fb5a4cb78a0b64dbb6ea5828fd87ea9
ketangote <ketan.gote@metamagic.in>
2017-08-18 17:33:55 +0530

    Padding added for datapoints.

--- src/components/datapoints/datapoints.component.ts
+++ src/components/datapoints/datapoints.component.ts
@@ -11,29 +11,32 @@ import {
 @Component({
   selector: 'amexio-datapoints',
   template: `
-    <table width="100%"  [style.background-color]="backgroundColor" [style.color]="fontColor">
-      <tr *ngIf="north">
-        <td [attr.colspan]="colspan">
-          <ng-content select="amexio-north"></ng-content>
-        </td>
-      </tr>
-      <tr>
-        <td *ngIf="west">
-          <ng-content select="amexio-west"></ng-content>
-        </td>
-        <td *ngIf="center">
-          <ng-content select="amexio-center"></ng-content>
-        </td>
-        <td *ngIf="east">
-          <ng-content select="amexio-east"></ng-content>
-        </td>
-      </tr>
-      <tr  *ngIf="south">
-        <td [attr.colspan]="colspan">
-          <ng-content select="amexio-south"></ng-content>
-        </td>
-      </tr>
-    </table>
+    <div style="padding: 5px;">
+      <table width="100%"  [style.background-color]="backgroundColor" [style.color]="fontColor">
+        <tr *ngIf="north">
+          <td [attr.colspan]="colspan">
+            <ng-content select="amexio-north"></ng-content>
+          </td>
+        </tr>
+        <tr>
+          <td *ngIf="west">
+            <ng-content select="amexio-west"></ng-content>
+          </td>
+          <td *ngIf="center">
+            <ng-content select="amexio-center"></ng-content>
+          </td>
+          <td *ngIf="east">
+            <ng-content select="amexio-east"></ng-content>
+          </td>
+        </tr>
+        <tr  *ngIf="south">
+          <td [attr.colspan]="colspan">
+            <ng-content select="amexio-south"></ng-content>
+          </td>
+        </tr>
+      </table>
+    </div>
+
 
 
   `
